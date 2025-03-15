bad4ead31ba906b5a34969a19d5f4d15e497bbc7
Rhuan Silva De Oliveira <202503535681@alunos.estacio.br>
2025-03-15 15:35:06 +0000

    logicaSuperTrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,122 +1,76 @@
 #include <stdio.h>
 
-int main(){
-    //denominação das variaveis
-
-    char codigo1[30], codigo2[30], estado1[40], estado2[40], cidade1[75], cidade2[75];
+int main() {
+    // Definição das variáveis
+    char estado1, estado2, codigo1[3], codigo2[3], cidade1[75], cidade2[75];
     float area1, area2, pib1, pib2;
     int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
 
-    //insira os dados da carta 01
+    // Inserção dos dados da Carta 1
+    printf("Digite uma letra de A a H para simbolizar o primeiro estado:\n");
+    scanf(" %c", &estado1); // Adicionado espaço antes do %c para evitar problemas com buffer
 
-    printf("Digite uma letra de A a H para simbolizar o primeiro estado. \n");
-    printf("Estado:\n");
-    printf("%c, &estado1\n");
-  
-    printf("Digite um núnmero entre 01 e 04 para determinar o número da carta. \n");
-    printf("Numero: \n");
-    scanf("%2s, codigo1\n");
+    printf("Digite um número entre 01 e 04 para determinar o número da carta:\n");
+    scanf("%2s", codigo1);
 
-    printf("Digite o nome da primeira cidade.\n");
-    printf("Nome da cidade:\n");
-    getchar();
+    printf("Digite o nome da primeira cidade:\n");
+    getchar();  // Limpar buffer
     fgets(cidade1, sizeof(cidade1), stdin);
 
-    printf("Informe o número de habitantes da cidade.\n");
-    printf("População:\n");
-    scanf("%d, &populacao1\n");
+    printf("Informe o número de habitantes da cidade:\n");
+    scanf("%d", &populacao1);
 
-    printf("Informe a área territorial em km² da cidade.\n");
-    printf("Área em km²:\n");
-    scanf("%d, &area1\n");
+    printf("Informe a área territorial em km² da cidade:\n");
+    scanf("%f", &area1);
 
-    printf("Informe o valor do PIB da cidade:\n");
-    printf("PIB:\n");
-    scanf("%d, &pib1\n");
+    printf("Informe o valor do PIB da cidade (em bilhões de reais):\n");
+    scanf("%f", &pib1);
 
-    printf("Informe quantos pontos turisticos tem a cidade.\n");
-    printf("Pontos turisticos:\n");
-    scanf("%d, &pontos_turisticos1\n");
+    printf("Informe quantos pontos turísticos tem a cidade:\n");
+    scanf("%d", &pontos_turisticos1);
 
-    // informe os dados da carta 02
+    // Inserção dos dados da Carta 2
+    printf("\nDigite uma letra de A a H para simbolizar o segundo estado:\n");
+    scanf(" %c", &estado2);
 
-    printf("Digite uma letra de A a H para simbolizar o sgundo estado.\n");
-    printf("Estado: \n");
-    scanf("%c, &estado2\n");
+    printf("Digite um número entre 01 e 04 para determinar o número da carta:\n");
+    scanf("%2s", codigo2);
 
-    printf("Digite um número entre 01 e 04 determinar o numero da carta.\n");
-    printf("Numero: \n");
-    scanf("%2s, codigo2\n");
-
-    printf("Digite o neme da segunda cidade:");
-    printf("Nome da cidade: \n");
-    getchar();
+    printf("Digite o nome da segunda cidade:\n");
+    getchar();  // Limpar buffer
     fgets(cidade2, sizeof(cidade2), stdin);
 
-    printf("Informe o número de habitantes da cidade.\n");
-    printf("População: \n");
-    scanf("%d, &populacao2");
+    printf("Informe o número de habitantes da cidade:\n");
+    scanf("%d", &populacao2);
 
-    printf("Informe a área territorial em km² da cidade.\n");
-    print("Área em km² \n");
-    scanf("%d, &area2\n");
+    printf("Informe a área territorial em km² da cidade:\n");
+    scanf("%f", &area2);
 
-    printf("Informe o valor do PIB da cidade.");
-    printf("PIB: \n");
-    scanf("%d, &pib2\n");
+    printf("Informe o valor do PIB da cidade (em bilhões de reais):\n");
+    scanf("%f", &pib2);
 
-    printf("Informe quantos pontos turisticos tem a cidade.\n");
-    printf("Pontos turisticos: \n");
-    scanf("%d, &pontos_turisticos2\n");
+    printf("Informe quantos pontos turísticos tem a cidade:\n");
+    scanf("%d", &pontos_turisticos2);
 
+    // Exibição dos dados da Carta 1
+    printf("\n--- Dados da Carta 1 ---\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %c%s\n", estado1, codigo1);
+    printf("Nome da Cidade: %s", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos1);
 
-
-
-    printf("Digite uma letra de A a H para simbolizar o segundo estado.\n");
-    printf("Estado: ");
-    scanf("%c, &estado2 ");
-
-    printf("Digite um número entre 01 e 04 determinar o numero da carta.\n");
-    printf("Numero: ");
-    scanf("%2s, codigo2 ");
-
-    printf("Digite o neme da segunda cidade.\n");
-    printf("Nome da cidade: \n");
-    getchar();
-    fgets(cidade2, sizeof(cidade2), stdin);
-
-    printf("Informe o número de habitantes da cidade.\n");
-    printf("População: ");
-    scanf("%d, &populacao2 ");
-
-    printf("Informe a área territorial em km² da cidade.\n");
-    printf("Área em km² ");
-    scanf("%f, &area2 ");
-
-    printf("Informe o valor do PIB da cidade.\n");
-    printf("PIB: ");
-    scanf("%f, &pib2 ");
-
-    printf("Informe quantos pontos turisticos tem a cidade.\n");
-    printf("Pontos turisticos: ");
-    scanf("%d, &pontos_turisticos2 ");
-
-    // Dados da carta 0
-
-    printf("Estado: '%c' \n", estado1);
-    printf("Codigo: %c \n", estado1, codigo1);
-    printf("Nome da cidade: %s", cidade1);
-    printf("Populacao: %d \n", populacao1);
-    printf("Area em km²: %.2f \n", area1);
-    printf("PIB: %.2f bilhões de reais/n", pib1);
-    printf("Pontos turisticos: %d \n", pontos_turisticos1);
-
-
-
-
-
-
-
+    // Exibição dos dados da Carta 2
+    printf("\n--- Dados da Carta 2 ---\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %c%s\n", estado2, codigo2);
+    printf("Nome da Cidade: %s", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos2);
 
     return 0;
 }

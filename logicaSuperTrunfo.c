a0b57d34d8a0be5fd848fe796ab316330ba91660
Rhuan Silva De Oliveira <202503535681@alunos.estacio.br>
2025-03-15 14:27:31 +0000

    alteracoes

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -22,11 +22,11 @@ int main(){
     getchar();
     fgets(cidade1, sizeof(cidade1), stdin);
 
-    printf("Informe o número habitantes da cidade.\n");
+    printf("Informe o número de habitantes da cidade.\n");
     printf("População:\n");
-    scanf("%f, &populacao1\n");
+    scanf("%d, &populacao1\n");
 
-    printf("Informe a área territorial da cidade.\n");
+    printf("Informe a área territorial em km² da cidade.\n");
     printf("Área em km²:\n");
     scanf("%d, &area1\n");
 
@@ -43,6 +43,31 @@ int main(){
     printf("Digite uma letra de A a H para simbolizar o sgundo estado.\n");
     printf("Estado: \n");
     scanf("%c, &estado2\n");
+
+    printf("Digite um número entre 01 e 04 determinar o numero da carta.\n");
+    printf("Numero: \n");
+    scanf("%2s, codigo2\n");
+
+    printf("Digite o neme da segunda cidade:");
+    printf("Nome da cidade: \n");
+    getchar();
+    fgets(cidade2, sizeof(cidade2), stdin);
+
+    printf("Informe o número de habitantes da cidade.\n");
+    printf("População: \n");
+    scanf("%d, &populacao2");
+
+    printf("Informe a área territorial em km² da cidade.\n");
+    print("Área em km² \n");
+    scanf("%d, &area2\n");
+
+    printf("Informe o valor do PIB da cidade.");
+    printf("PIB: \n");
+    scanf("%d, &pib2\n");
+
+    printf("Informe quantos pontos turisticos tem a cidade.\n");
+    printf("Pontos turisticos: \n");
+    scanf("%d, &pontos_turisticos2\n");
 
    
 

c5fb5fd7bad50108457b6a0c90f9149823bff2c3
Rhuan Silva De Oliveira <202503535681@alunos.estacio.br>
2025-03-15 00:37:45 +0000

    alteracao

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main() {
-  char estado1, estado2
+int main(){
 
-    return 0;
+    char codigo1[10], codigo2[10], estado1[40], estado2[40], cidade1[75], cidade2[75];
+    float area1, area2, pib1, pib2;
+    int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
+
+    printf("Digite uma letra de A a H para simbolizar o primeiro estado \n ");
+    printf("Estado: \n");
+    scanf(" %c, &estado1");
+
+    printf("Escolha entre 01 e 04 para determinar o número da carta \n");
+    printd("")
+
+
+
+
 }
